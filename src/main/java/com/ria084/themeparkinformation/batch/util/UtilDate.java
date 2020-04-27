0edfa0b26c07c2e6bebc28464023879b5b0a6d89
Iria <siki.fate18.cat@gmail.com>
2020-04-27 11:06:34 +0900

    日付操作クラスの修正

--- src/main/java/com/ria084/themeparkinformation/batch/util/UtilDate.java
+++ src/main/java/com/ria084/themeparkinformation/batch/util/UtilDate.java
@@ -5,29 +5,38 @@ import org.springframework.stereotype.Service;
 import java.time.LocalDate;
 import java.time.YearMonth;
 import java.time.format.DateTimeFormatter;
+import java.time.format.ResolverStyle;
 
 @Service
 public class UtilDate {
 
-    public static final DateTimeFormatter yearmonthFormatter = DateTimeFormatter.ofPattern("uuuuMM");
+    public static final DateTimeFormatter yearmonthFormatter = DateTimeFormatter.ofPattern("uuuuMM").withResolverStyle(ResolverStyle.STRICT);
 
-    public static LocalDate get1stDateOfMonth(String yearMonth){
+    public static final DateTimeFormatter datetimeFormatter = DateTimeFormatter.ofPattern("uuuuMMdd").withResolverStyle(ResolverStyle.STRICT);
+
+
+    public static LocalDate get1stDateOfMonth(String yearMonth) {
         return get1stDateOfMonth(YearMonth.parse(yearMonth, yearmonthFormatter));
-
     }
 
-    public static LocalDate get1stDateOfMonth(YearMonth yearMonth){
+    public static LocalDate get1stDateOfMonth(YearMonth yearMonth) {
         return yearMonth.atDay(1);
     }
 
-    public static LocalDate getLastDateOfMonth(String yearMonth){
+    public static LocalDate getLastDateOfMonth(String yearMonth) {
         return getLastDateOfMonth(YearMonth.parse(yearMonth, yearmonthFormatter));
-
     }
 
-    public static LocalDate getLastDateOfMonth(YearMonth yearMonth){
+    public static LocalDate getLastDateOfMonth(YearMonth yearMonth) {
         return yearMonth.atEndOfMonth();
     }
 
+    public static LocalDate parseStringToDate(String localDate) {
+        return LocalDate.parse(localDate, datetimeFormatter);
+    }
+
+    public static String parseLocalDateToString(LocalDate date) {
+        return date.format(datetimeFormatter);
+    }
 
 }
